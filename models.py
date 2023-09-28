b15f1aebb302782e328e126c33f9d77682ac3a92
bobclub1996 <70020651+bobclub1996@users.noreply.github.com>
2023-09-29 00:45:12 +0400

    Настройка безопасности

--- models.py
+++ models.py
@@ -1,15 +1,17 @@
-import os
+from os import getenv
+from dotenv import load_dotenv, find_dotenv
 
 from sqlalchemy import JSON, Column, Integer, String
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-PG_USER = os.getenv("PG_USER", "postgres")
-PG_PASSWORD = os.getenv("PG_PASSWORD", "123456")
-PG_DB = os.getenv("PG_DB", "netology_asy")
-PG_HOST = os.getenv("PG_HOST", "127.0.0.1")
-PG_PORT = os.getenv("PG_PORT", "5432")
+load_dotenv(find_dotenv())
+PG_USER = getenv("PG_USER")
+PG_PASSWORD = getenv("PG_PASSWORD")
+PG_DB = getenv("PG_DB")
+PG_HOST = getenv("PG_HOST")
+PG_PORT = getenv("PG_PORT")
 
 PG_DSN = f"postgresql+asyncpg://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}"
 
